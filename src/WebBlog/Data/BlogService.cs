3a5f9b8748a933febeebff4f53b3117b7d1e3d08
Simon Foster <funkysi1701@gmail.com>
2020-11-12 16:33:32 +0000

    only display what is on screen

--- src/WebBlog/Data/BlogService.cs
+++ src/WebBlog/Data/BlogService.cs
@@ -31,25 +31,6 @@ namespace WebBlog.Data
             return posts;
         }
 
-        public async Task<List<BlogPosts>> GetBlogsAsync(int start, int count)
-        {
-            var call = Client.GetAsync(new Uri(Client.BaseAddress + "articles/me?per_page=100"));
-            HttpResponseMessage httpResponse = await call;
-
-            string result = await httpResponse.Content.ReadAsStringAsync();
-            List<BlogPosts> posts = JsonConvert.DeserializeObject<List<BlogPosts>>(result);
-            httpResponse.Dispose();
-            if (posts[start] == null)
-            {
-                return posts.OrderByDescending(x => x.Published_At).ToList();
-            }
-            else if(posts.OrderByDescending(x => x.Published_At).Where(y => y.Published_At <= posts[start].Published_At).Count() > count)
-            {
-                return posts.OrderByDescending(x => x.Published_At).Where(y => y.Published_At <= posts[start].Published_At).Take(count).ToList();
-            }
-            else return posts.OrderByDescending(x => x.Published_At).Where(y => y.Published_At <= posts[start].Published_At).ToList();
-        }
-
         public async Task<BlogPosts> GetBlogPostAsync(int id)
         {
             var call = Client.GetAsync(new Uri(Client.BaseAddress + "articles/" + id.ToString()));
