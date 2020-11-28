350ed7565a0b9f915c400772ceb4416835a9997f
Simon Foster <funkysi1701@gmail.com>
2020-11-28 22:47:47 +0000

    update page title

---
+++ src/WebBlog/Components/PageTitleGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBlog.Components
+{
+    public static class PageTitleGenerator
+    {
+        /// <summary>
+        /// Create Page Title
+        /// </summary>
+        /// <param name="title"></param>
+        /// <returns></returns>
+        public static string Create(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(title));
+            
+
+            title = title.Replace('-', ' ');
+            if (title.Contains("posts"))
+            {
+                title = title.Replace("posts", "");
+            }
+            title = title.Substring(0, title.Length - 4);
+            string pageTitle = title switch
+            {
+                "/" => string.Empty,
+                _ => $"{title}",
+            };
+            return pageTitle + " - Funky Si's Tech Talk";
+        }
+    }
+}
---
+++ src/WebBlog/wwwroot/js/headers.js
@@ -0,0 +1,15 @@
+﻿var JsFunctions = window.JsFunctions || {};
+
+JsFunctions = {
+    setDocumentTitle: function (title) {
+        document.title = title;
+        var meta = document.createElement('meta');
+        meta.name = "twitter:title";
+        meta.content = title.trim();
+        document.getElementsByTagName('head')[0].appendChild(meta);
+        var meta = document.createElement('meta');
+        meta.name = "twitter:url";
+        meta.content = window.location.href;
+        document.getElementsByTagName('head')[0].appendChild(meta);
+    }
+};
