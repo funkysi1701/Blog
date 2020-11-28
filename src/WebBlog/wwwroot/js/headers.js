3832bc805af46f9eef73a7cb4cb231ace98eeb8a
Simon Foster <funkysi1701@gmail.com>
2020-11-28 23:01:09 +0000

    more header tweaking

--- src/WebBlog/wwwroot/js/headers.js
+++ src/WebBlog/wwwroot/js/headers.js
@@ -4,12 +4,24 @@ JsFunctions = {
     setDocumentTitle: function (title) {
         document.title = title;
         var meta = document.createElement('meta');
+        meta.name = "twitter:card";
+        meta.content = "summary_large_image";
+        document.getElementsByTagName('head')[0].appendChild(meta);
+        var meta = document.createElement('meta');
         meta.name = "twitter:title";
         meta.content = title.trim();
         document.getElementsByTagName('head')[0].appendChild(meta);
         var meta = document.createElement('meta');
+        meta.name = "twitter:description";
+        meta.content = "";
+        document.getElementsByTagName('head')[0].appendChild(meta);
+        var meta = document.createElement('meta');
         meta.name = "twitter:url";
         meta.content = window.location.href;
+        document.getElementsByTagName('head')[0].appendChild(meta);
+        var meta = document.createElement('meta');
+        meta.name = "twitter:site";
+        meta.content = "@funkysi1701";
         document.getElementsByTagName('head')[0].appendChild(meta);
     }
 };
