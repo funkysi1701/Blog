3927fd43463f8d0611919091f20e45b41cae4351
Simon Foster <funkysi1701@gmail.com>
2020-12-06 22:20:15 +0000

    tw card

--- src/WebBlog/wwwroot/js/headers.js
+++ src/WebBlog/wwwroot/js/headers.js
@@ -4,6 +4,18 @@ JsFunctions = {
     setDocumentTitle: function (title) {
         document.title = title;
         var meta = document.createElement('meta');
+        meta.name = "twitter:card";
+        meta.content = "summary";
+        document.getElementsByTagName('head')[0].appendChild(meta);
+        var meta = document.createElement('meta');
+        meta.name = "twitter:site";
+        meta.content = "@funkysi1701";
+        document.getElementsByTagName('head')[0].appendChild(meta);
+        var meta = document.createElement('meta');
+        meta.name = "twitter:image";
+        meta.content = "https://www.funkysi1701.com/wp-content/uploads/2014/09/1922276.jpg";
+        document.getElementsByTagName('head')[0].appendChild(meta);
+        var meta = document.createElement('meta');
         meta.name = "twitter:title";
         meta.content = title.trim();
         document.getElementsByTagName('head')[0].appendChild(meta);
