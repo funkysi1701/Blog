2282e1798196da6c6310273cb7b661c11b11b048
Simon Foster <funkysi1701@gmail.com>
2021-01-02 14:16:07 +0000

    add twitter metrics

--- src/WebBlog/Data/BlogService.cs
+++ src/WebBlog/Data/BlogService.cs
@@ -4,17 +4,20 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Tweetinvi;
 
 namespace WebBlog.Data
 {
     public class BlogService
     {
         private HttpClient Client { get; set; }
+        private TwitterClient UserClient { get; set; }
 
         public BlogService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
         {
             Client = httpClientFactory.CreateClient("BlogClient");
             Client.DefaultRequestHeaders.Add("api-key", configuration.GetValue<string>("DEVTOAPI"));
+            UserClient = new TwitterClient(configuration.GetValue<string>("TWConsumerKey"), configuration.GetValue<string>("TWConsumerSecret"), configuration.GetValue<string>("TWAccessToken"), configuration.GetValue<string>("TWAccessSecret"));
         }
 
         public async Task<List<BlogPosts>> GetBlogsAsync()
@@ -39,6 +42,19 @@ namespace WebBlog.Data
             httpResponse.Dispose();
 
             return posts;
+        }
+
+        public async Task<int> GetTwitterFollowers()
+        {
+            var followers = (await UserClient.Users.GetFollowerIdsAsync("funkysi1701")).Length;
+            
+            return followers;
+        }
+        public async Task<int> GetTwitterFollowing()
+        {
+            var friends = (await UserClient.Users.GetFriendIdsAsync("funkysi1701")).Length;
+            
+            return friends;
         }
     }
 }
