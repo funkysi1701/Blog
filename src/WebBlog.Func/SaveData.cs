66bd059511fac0c5e0328c88442e49d90b4ae2ae
Simon Foster <funkysi1701@gmail.com>
2021-01-11 21:14:01 +0000

    add function

---
+++ src/WebBlog.Func/SaveData.cs
@@ -0,0 +1,24 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+
+namespace WebBlog.Func
+{
+    public static class SaveData
+    {
+        [FunctionName("SaveData")]
+        public static void Run([TimerTrigger("0 0 * * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            var client = new HttpClient();
+            client.GetAsync(config.GetValue<string>("URL"));
+        }
+    }
+}
